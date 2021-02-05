b840d1797f27bf09ed7241579741a92922e86886
Øyvind <yvind@10.0.0.72>
2021-02-05 13:03:21 +0100

    Oppg1 ferdig

--- dat110-project1/src/no/hvl/dat110/messaging/MessagingServer.java
+++ dat110-project1/src/no/hvl/dat110/messaging/MessagingServer.java
@@ -25,15 +25,23 @@ public class MessagingServer {
 
 	// accept an incoming connection from a client
 	public Connection accept() {
-
+		Socket connectionSocket; 
 		Connection connection = null;
-
+		
 		// TODO
 		// accept TCP connection on welcome socket and create messaging connection
-
-		if (true) {
-			throw new UnsupportedOperationException(TODO.method());
+		
+		try {
+			connectionSocket = welcomeSocket.accept();
+			
+			connection = new Connection(connectionSocket); 
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+		
 
 		return connection;
 
